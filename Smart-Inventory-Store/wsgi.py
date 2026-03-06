44d43e98676752c104714b0e592b6b0a95966d43
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-06 16:27:29 +0200

    Update wsgi.py

--- Smart-Inventory-Store/wsgi.py
+++ Smart-Inventory-Store/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for Smart-Inventory-Store project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Smart-Inventory-Store.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    os.getenv("DJANGO_SETTINGS_MODULE", "Smart_Inventory_Store.settings.prod")
+)
 
 application = get_wsgi_application()
